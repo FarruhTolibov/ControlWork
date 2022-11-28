9f23b441393c5a291c8eb1a156395e91b925bf20
Farruh Talibov <ferruh9talibov@gmail.com>
2022-11-28 17:01:26 +0500

    CountLessThan

--- ContolWork/solution/Program.cs
+++ ContolWork/solution/Program.cs
@@ -7,3 +7,15 @@ string[] AskArray() {
     Write("Введите значения через пробел: ");
     return ReadLine().Split(" ");
 }
+
+int CountLessThan(string[] input, int n) {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
